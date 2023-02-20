c9ed699d7f42316fe530335743a399f6dc6229b7
Jhonattan Lino <jhonattanlino@outlook.com>
2023-02-20 15:53:44 -0300

    Update Code.R

--- Code.R
+++ Code.R
@@ -4,7 +4,6 @@ library(timetk)  ## library to detection anomaly
 library(readxl)
 library(dplyr)
 library(lubridate)
-library(xts)
 
 
 exc <- readxl::read_excel('SPGlobal_Brazil_CurrencyExchangeRates_20-Feb-2023.xls', 
@@ -14,8 +13,12 @@ str(exc)
 
 colnames(exc) <- c('Date', 'argentine', 'brazil', 'Chile', 'colombian', 'mexican', 'uruguay')
 
+exc %>%
+  mutate(pct_arg = (lag(argentine)-argentine)/argentine*100)
 
 exc %>%
-  #group_by(year(Date)) %>%
-  timetk::plot_anomaly_diagnostics(Date, argentine, .facet_scales='free', .interactive=TRUE)
- 
+  mutate(exc_arge = (lag(argentine) - argentine)/argentine) %>%
+  group_by(year(Date)) %>%
+  timetk::plot_seasonal_diagnostics(Date, exc_arge, .feature_set = c('week', 'month.lbl', 'quarter'))
+  
+View(exc)
